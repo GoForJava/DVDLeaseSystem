170820d8adc712aed0914a55f5413504a24ede2e
qianjiaxiang <895239330@qq.com>
2017-01-24 21:27:23 +0800

    this is some changes

--- src/com/dvd/dao/RecordDao.java
+++ src/com/dvd/dao/RecordDao.java
@@ -1,10 +1,11 @@
 package com.dvd.dao;
 
 import com.dvd.entity.Record;
+import com.dvd.entity.Record2;
 
 public interface RecordDao {
 	public Record queryRecoreById(int rid);//通过制定id查看制定id的DVD借还记录
 	public boolean saveRecord(Record record);//保存record记录
-	public boolean updateRecord(Record record);//更新record
+	public boolean updateRecord(Record2 record2);//更新record
 	
 }
