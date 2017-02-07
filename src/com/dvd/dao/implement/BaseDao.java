fa260d15031b018308a6660fdc9e083df9aa6763
qianjiaxiang <895239330@qq.com>
2017-02-07 12:37:23 +0800

    jdbc some changes

--- src/com/dvd/dao/implement/BaseDao.java
+++ src/com/dvd/dao/implement/BaseDao.java
@@ -1,9 +1,17 @@
 package com.dvd.dao.implement;
 
+import java.sql.DriverManager;
+
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
 public class BaseDao {
 	Connection con;
 	private static final String DRIVER= "com.mysql.jdbc.Driver";
+	String url = "jdbc:mysql://127.0.0.1:3306/dvd";
+	String usr = "root";
+	Connection conn = null;
+	String sql = null;
+	String password = "admin";
 	
 }
